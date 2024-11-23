3f3228bf60f150065f85afec650458db8d2d2ed5
AnjaneyuluDaddala <anjaneyuludaddala07@gmail.com>
2024-11-23 18:25:13 +0530

    initial-commit

---
+++ src/test/java/com/java/spring_boot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
